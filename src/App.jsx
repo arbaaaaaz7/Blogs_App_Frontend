d7397b4d50c73abe073dc191f7af71d266df7916
Arbas <arbazarb.z07@gmail.com>
2025-02-20 14:28:40 +0530

    navbar partial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { Profiler, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import MyBlogs from './pages/MyBlogs'
+import CreatePost from './pages/CreatePost'
+import PostDetails from './pages/PostDetails'
+import EditPost from './pages/EditPost'
+import Profile from './pages/Profile'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -10,6 +17,14 @@ function App() {
     <>
     <Routes>
       <Route exact path='/' element={ <Home/> } />
+      <Route exact path='/login' element={ <Login/> } />
+      <Route exact path='/register' element={ <Register/> } />
+      <Route exact path='/write' element={ <CreatePost/> } />
+      <Route exact path='/Post/post/:id' element={ <PostDetails/> } />   
+      <Route exact path='/edit/:id' element={ <EditPost/> } />
+      <Route exact path='/myblogs/:id' element={ <MyBlogs/> } />
+      <Route exact path='/profile/:id' element={ <Profile/> } />
+
       
     </Routes>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useLocation, useNavigation } from 'react-router-dom'
+import BsSearch from 'react-icons/bs'
+import FaBars from 'react-icons/fa'
 
 function Navbar() {
+ const [prompt, setPrompt] = useState("")
+ const [menu, setMenu] = useState(false)
+ const navigate = useNavigation()
+ const path = useLocation() .pathname
+
+
+
   return (
-    <div>Navbar</div>
+    <div>
+      <div className=' flex items-center justify-between px-6 md:px-[200px] py-4 bg-black text-white'>
+      <h1 className='text-lg md:text-xl font-extrabold'> 
+        <Link to="/">Blogosphere </Link> </h1>
+        { path ==="/" && <div onChange={(e) => setPrompt(e.target.value) } className='flex justify-center items-center space-x-0'>
+<input type="text" className='outline-none rounded-1-xl px-3 text-black bg-white' placeholder='Search a pst' />
+<p onClick={() => navigate(prompt ? "search" + prompt : navigate ("/")) } className='cursor-pointer p-1 bg-white text-black rounded-r-xl '>
+  <BsSearch/>
+</p>
+        </div> }
+
+<div className='hidden md:flex items-center justify-center space-x-2 md:space-x-4'>
+  {
+    user ? <h3> <Link to='/write'>Write</Link>  </h3> : <h3>
+      <Link to='/login'>Login</Link>
+    </h3>
+  }
+{user? <div onClick={showMenu} >
+  <p className='cursor-pointer relative'>
+    <FaBars/>
+  </p>
+
+
+</div> }  
+
+</div>
+
+      </div>
+      
+
+    </div>
   )
 }
 
